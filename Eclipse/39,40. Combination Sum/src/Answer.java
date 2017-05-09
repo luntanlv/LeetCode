afec09e76001fde38d142742318a411cfd641fbd
luntanlv <luntanlv@gmail.com>
2017-05-09 14:50:37 -0400

    39 40

--- Eclipse/39,40. Combination Sum/src/Answer.java
+++ Eclipse/39,40. Combination Sum/src/Answer.java
@@ -3,14 +3,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Answer {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+	public static void main(String [] args)
+	{
+        Answer answer = new Answer();  
+        System.out.println();
+	}	
+	
+	//39
+    public List<List<Integer>> combinationSum39(int[] candidates, int target) {
     	Arrays.sort(candidates);
     	List<List<Integer>> res = new ArrayList<>();
-    	helper(candidates, target, new ArrayList<Integer>(), 0, 0, res);
+    	helper39(candidates, target, new ArrayList<Integer>(), 0, 0, res);
     	return res;
     }
     
-    private void helper(int[] candidatesSort, int target, List<Integer> currentList, int currentSum, int currentIndex, List<List<Integer>> res){
+    private void helper39(int[] candidatesSort, int target, List<Integer> currentList, int currentSum, int currentIndex, List<List<Integer>> res){
     	if(currentSum>target){
     		return;
     	}
@@ -22,8 +29,31 @@ public class Answer {
     	for(int i=currentIndex; i<candidatesSort.length; i++){
     		List<Integer> temp = new ArrayList<Integer>(currentList);
     		temp.add(candidatesSort[i]);
-    		helper(candidatesSort, target, temp, currentSum+candidatesSort[i], i, res);
-    	}
-    	    	
+    		helper39(candidatesSort, target, temp, currentSum+candidatesSort[i], i, res);
+    	}    	
     }
+    
+    //40
+    public List<List<Integer>> combinationSum40(int[] candidates, int target) {
+    	//Arrays.sort(candidates);
+    	List<List<Integer>> res = new ArrayList<>();
+    	helper40(candidates, target, new ArrayList<Integer>(), 0, 0, res);
+    	return res;
+    }
+    
+    private void helper40(int[] candidatesSort, int target, List<Integer> currentList, int currentSum, int currentIndex, List<List<Integer>> res){
+    	if(currentSum>target){
+    		return;
+    	}
+    	
+    	else if(currentSum == target){
+    		res.add(currentList);
+    	}
+    	
+    	for(int i=currentIndex; i<candidatesSort.length; i++){
+    		List<Integer> temp = new ArrayList<Integer>(currentList);
+    		temp.add(candidatesSort[i]);
+    		helper40(candidatesSort, target, temp, currentSum+candidatesSort[i], i, res);
+    	}    	
+    }    
 }
