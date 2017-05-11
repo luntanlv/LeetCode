67c64b3be202a4ccd211bcad34ca830ac6f1e795
luntanlv <luntanlv@gmail.com>
2017-05-10 23:41:45 -0400

    224 newer solution

--- Eclipse/224. Basic Calculator/src/Answer.java
+++ Eclipse/224. Basic Calculator/src/Answer.java
@@ -6,13 +6,99 @@ public class Answer {
 	public static void main(String [] args)
 	{
         Answer answer = new Answer();
-        String input = "(5-(1+(5)))";
+        String input = "1-((1+1))";
         
         int res = answer.calculate(input);
         System.out.println(res);
 	}
 	
     public int calculate(String s) {
+    	Stack<Integer> numSt = new Stack<Integer>();
+    	Stack<Character> opSt = new Stack<Character>();
+    	
+    	int tempNum=0;
+    	boolean getNum = false;
+        for(int i=0; i<s.length(); i++){
+        	char cur = s.charAt(i);
+        	
+			if((cur<'0' || cur>'9') && getNum){
+				//no pending op right now
+				if(opSt.isEmpty() || opSt.peek() == '(')
+					numSt.push(tempNum);
+				// pending op can be done right now
+				else{
+		    		char pendingOp = opSt.pop();
+		    		int num = numSt.pop();
+		    		switch(pendingOp){
+		    			case '+':
+		    				numSt.push(num+tempNum);
+		    				break;
+		    			case '-':
+		    				numSt.push(num-tempNum);
+		    				break;
+		    		}   
+				}
+				tempNum = 0;
+				getNum=false;
+			}
+			
+			if(cur>='0' && cur<='9'){
+				getNum = true;
+				tempNum=tempNum*10+cur-'0';
+			}  	        	
+        	
+			else if(cur == '('){
+				opSt.push('(');
+        	}
+        	else if(cur ==')'){    		
+        		//pop '('
+        		opSt.pop();
+        		//get previous pending op (op before '(')
+        		if(!opSt.isEmpty() && opSt.peek() != '('){
+    	    		char pendingOp = opSt.pop();
+    	    		int num1 = numSt.pop();
+    	    		int num2 = numSt.pop();
+    	    		switch(pendingOp){
+    	    			case '+':
+    	    				numSt.push(num1+num2);
+    	    				break;
+    	    			case '-':
+    	    				numSt.push(num2-num1);
+    	    				break;
+    	    		}         			
+        		}
+  
+			}
+        	else if(cur =='+' || cur =='-')
+        		opSt.push(cur);
+        	else
+        		continue;
+        }
+        
+        if(getNum){
+        	numSt.push(tempNum);
+        	if(!opSt.isEmpty()){
+	    		char pendingOp = opSt.pop();
+	    		int num1 = numSt.pop();
+	    		int num2 = numSt.pop();
+	    		switch(pendingOp){
+	    			case '+':
+	    				numSt.push(num1+num2);
+	    				break;
+	    			case '-':
+	    				numSt.push(num2-num1);
+	    				break;
+	    		}         		
+        	}
+        	
+        }     	
+        
+        return numSt.pop();
+    }	
+	
+	
+	
+    public int calculate2(String s) {
     	Stack<String> tempRes = new Stack<String>();
     	tempRes.push("");
 
