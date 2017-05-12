0bd991a957eea2c1309b3240298d2dee702a14e0
luntanlv <luntanlv@gmail.com>
2017-05-12 14:55:17 -0400

    498

--- Eclipse/54. Spiral Matrix 498. Diagonal Traverse/src/Answer.java
+++ Eclipse/54. Spiral Matrix 498. Diagonal Traverse/src/Answer.java
@@ -5,9 +5,13 @@ public class Answer {
 	public static void main(String [] args)
 	{
         Answer answer = new Answer(); 
-        int[][] input = {{1}};
-        List<Integer> res = answer.spiralOrder(input);
-        System.out.println(res);
+        int[][] input = {{1,2,3},{4,5,6},{7,8,9}};
+        //List<Integer> res = answer.spiralOrder(input);
+        //System.out.println(res);
+        int[] res = answer.findDiagonalOrder(input);
+        for(int item: res){
+        	System.out.print(item + ", ");
+        }
 	}
 	
 	public enum Direction {Ix,Iy,Dx,Dy};
@@ -64,5 +68,48 @@ public class Answer {
         }
 
         return res;
-    }	
+    }
+    
+    public int[] findDiagonalOrder(int[][] matrix) {
+    	
+        int m = matrix.length;
+        if(m==0)
+        	return new int[0];
+        int n=matrix[0].length;
+        
+        int[] res = new int[n*m]; 
+    	
+        boolean isUp = true;
+        int i=0,j=0;
+        for(int k=0; k<n*m;){
+            while(i>=0 && i<m && j>=0 && j<n){
+            	res[k]=matrix[i][j];
+            	if(isUp){
+            		i--;
+            		j++;
+            	}
+            	else{
+            		i++;
+            		j--;
+            	}
+            	k++;
+            }
+            
+            if(isUp){
+            	i=j<n? 0 : i+2;
+            	j=j==n? n-1 : j;
+            }
+            else{
+            	
+            	j=i<m? 0 : j+2;
+            	i=i==m? m-1 : i;
+            }
+            
+            isUp=!isUp;
+            
+        }
+        
+        return res;
+
+    }    
 }
