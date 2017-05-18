a1ddd9eff681e29ad336006fdca506b17de42580
luntanlv <luntanlv@gmail.com>
2017-05-18 19:50:46 -0400

    78

--- Eclipse/78. Subsets/src/Answer.java
+++ Eclipse/78. Subsets/src/Answer.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Answer {
 	public static void main(String [] args)
 	{
         Answer answer = new Answer();      
-        
-        List<List<Integer>> res = answer.combine(20,16);
+        int[] input = new int[]{1,2,3};
+        List<List<Integer>> res = answer.subsets(input);
         
         for(List<Integer> oneList: res){
         	for(int val: oneList)
@@ -16,27 +17,53 @@ public class Answer {
             
 	}
 	
-    public List<List<Integer>> subsets(int[] nums) {
+	//Backtracking
+	public List<List<Integer>> subsets(int[] nums) {
+	    List<List<Integer>> list = new ArrayList<>();
+	    Arrays.sort(nums);
+	    backtrack(list, new ArrayList<>(), nums, 0);
+	    return list;
+	}
+
+	private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+	    list.add(new ArrayList<>(tempList));
+	    for(int i = start; i < nums.length; i++){
+	        tempList.add(nums[i]);
+	        backtrack(list, tempList, nums, i + 1);
+	        tempList.remove(tempList.size() - 1);
+	    }
+	}
+    
+	
+
+	//
+    public List<List<Integer>> subsets2(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         res.add(new ArrayList<Integer>());
         
+    	int startOfPreLevel = 0;
+        int lenOfPreLevel = res.size();
+        
+        Arrays.sort(nums);
+        
         for(int i =0; i<nums.length; i++){
-        	int startOfPreLevel = 0;
-            int lenOfPreLevel = res.size();
             
             for(int j=startOfPreLevel; j<lenOfPreLevel; j++){
             	List<Integer> oneList = res.get(j);
+            	int lastNum = oneList.isEmpty()? Integer.MIN_VALUE: oneList.get(oneList.size()-1);
             	
             	for( int num : nums){
-            		if(!oneList.contains(num)){
+            		if(num>lastNum){
             			List<Integer> newList = new ArrayList<Integer>(oneList);
             			newList.add(num);
             			res.add(newList);
             		}
             	}
             	
-            }   	
+            }     
+            startOfPreLevel=lenOfPreLevel;
+            lenOfPreLevel = res.size();   
         }
-        
+        return res;
     }
 }
