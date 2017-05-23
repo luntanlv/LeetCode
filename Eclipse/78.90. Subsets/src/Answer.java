e335ba166d997533a9c5c5ce44029c5e5eddaaab
luntanlv <luntanlv@gmail.com>
2017-05-22 20:48:48 -0400

    78 90

--- Eclipse/78.90. Subsets/src/Answer.java
+++ Eclipse/78.90. Subsets/src/Answer.java
@@ -35,7 +35,8 @@ public class Answer {
 	    }
 	}
     
-	//
+	//loop by length of the res
+	//[] ->[],[1],[2],[3] -> [],[1],[2],[3],[1,2],[1,3],[2,3]
     public List<List<Integer>> subsets2(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         res.add(new ArrayList<Integer>());
@@ -66,6 +67,28 @@ public class Answer {
         return res;
     }
     
+    //loop by adding each item
+    //[] -> [], [1] -> [],[1], [2],[1,2] -> [],[1],[2],[1,2] [3],[1,3],[2,3],[1,2,3] 
+    public List<List<Integer>> subsets3(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        res.add(new ArrayList<Integer>());
+        
+        for(int i =0; i<nums.length; i++){
+        	
+        	int startOfPreLevel = 0;
+            int lenOfPreLevel = res.size();
+        	
+        	for(int j= startOfPreLevel; j<lenOfPreLevel; j++){
+        		List<Integer> oneList = res.get(j);
+        		List<Integer> newList = new ArrayList<Integer>(oneList);
+    			newList.add(nums[i]);
+    			res.add(newList);      		
+        	}
+        }
+        return res;        
+    }
+    
     
     //90
     //having dup
@@ -93,35 +116,36 @@ public class Answer {
 	    }
 	}
 	
-	
+    //loop by adding each item
+	//[1,2,3,4]
+    //[] -> [], [1] -> [],[1], [2],[1,2] -> [],[1],[2],[1,2],  [3],[1,3],[2,3],[1,2,3] ->[],[1],[2],[1,2], [3],[1,3],[2,3],[1,2,3],  [3,3],[1,3,3],[2,3,3],[1,2,3,3] 
     public List<List<Integer>> subsetsWithDup2(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         res.add(new ArrayList<Integer>());
-        
+
     	int startOfPreLevel = 0;
         int lenOfPreLevel = res.size();
         
         for(int i =0; i<nums.length; i++){
-            
-            for(int j=startOfPreLevel; j<lenOfPreLevel; j++){
-            	List<Integer> oneList = res.get(j);
-            	int lastNum = oneList.isEmpty()? Integer.MIN_VALUE: oneList.get(oneList.size()-1);       
-            	
-            	int preNum = Integer.MIN_VALUE;
-            	
-            	for( int num : nums){
-            		if(num>lastNum && num>preNum){
-            			preNum=num;
-            			List<Integer> newList = new ArrayList<Integer>(oneList);
-            			newList.add(num);
-            			res.add(newList);
-            		}
-            	}
-            	
-            }     
-            startOfPreLevel=lenOfPreLevel;
-            lenOfPreLevel = res.size();   
+        	
+        	//not an dup item
+        	if(i==0 || nums[i] != nums[i-1]){
+        		startOfPreLevel = 0;
+        		lenOfPreLevel = res.size();
+        	}
+        	//dup item, just add dup item to only last set
+        	else{
+        		startOfPreLevel = lenOfPreLevel;
+        		lenOfPreLevel = res.size();
+        	}
+        	
+        	for(int j= startOfPreLevel; j<lenOfPreLevel; j++){
+        		List<Integer> oneList = res.get(j);
+        		List<Integer> newList = new ArrayList<Integer>(oneList);
+    			newList.add(nums[i]);
+    			res.add(newList);      		
+        	}
         }
         return res;        
     }
