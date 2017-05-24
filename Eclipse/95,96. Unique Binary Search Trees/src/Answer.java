0d8abadcb8698d3b3ccfbd92be15f00798789189
luntanlv <luntanlv@gmail.com>
2017-05-24 13:42:28 -0400

    95

--- Eclipse/95,96. Unique Binary Search Trees/src/Answer.java
+++ Eclipse/95,96. Unique Binary Search Trees/src/Answer.java
@@ -6,7 +6,8 @@ public class Answer {
 	public static void main(String [] args)
 	{
         Answer answer = new Answer();      
-        List<TreeNode> res = answer.generateTrees(0);
+        //List<TreeNode> res = answer.generateTrees(0);
+        int res = answer.numTrees(4);
         System.out.println(res);       
 	}
 	
@@ -48,6 +49,21 @@ public class Answer {
     	
     	return res;
 
+    }
+    
+    //96
+    public int numTrees(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+        	for(int j=0; j<i; j++){
+        		dp[i]+=dp[j] * dp[i-j-1];
+        	}
+        }
+    	
+        return dp[n];
     }
 
     public class TreeNode {
