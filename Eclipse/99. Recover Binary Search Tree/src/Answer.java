f1c926c345333885abb81d5cb149bf4f7081031d
luntanlv <luntanlv@gmail.com>
2017-05-24 16:53:59 -0400

    99

---
+++ Eclipse/99. Recover Binary Search Tree/src/Answer.java
@@ -0,0 +1,26 @@
+import Answer.TreeNode;
+
+public class Answer {
+	public static void main(String [] args)
+	{
+        Answer answer = new Answer();
+        TreeNode root = answer.new TreeNode(2);
+        TreeNode left = answer.new TreeNode(1);
+        TreeNode right = answer.new TreeNode(3);
+        root.left=left;
+        root.right= right;
+        answer.recoverTree(root);
+        System.out.println(root);       
+	}
+	
+    public void recoverTree(TreeNode root) {
+        
+    }
+    
+    public class TreeNode {
+    	int val;
+    	TreeNode left;
+    	TreeNode right;
+    	TreeNode(int x) { val = x; }
+    }
+}
