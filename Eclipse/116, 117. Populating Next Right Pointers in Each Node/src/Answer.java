3876033181862c3bb10d61770ce43f17c635aa92
luntanlv <luntanlv@gmail.com>
2017-05-30 18:20:54 -0400

    117

--- Eclipse/116, 117. Populating Next Right Pointers in Each Node/src/Answer.java
+++ Eclipse/116, 117. Populating Next Right Pointers in Each Node/src/Answer.java
@@ -118,4 +118,32 @@ public class Answer {
     	}
     	return next;
     }
+    
+    public void connect_117_better(TreeLinkNode root) {
+        TreeLinkNode head=root;//The left most node in the lower level
+        TreeLinkNode prev=null;//The previous node in the lower level
+        TreeLinkNode curr=null;//The current node in the upper level
+        while (head!=null){
+            curr=head;
+            prev=null;
+            head=null;
+            while (curr!=null){
+                if (curr.left!=null){
+                    if (prev!=null)
+                        prev.next=curr.left;
+                    else 
+                        head=curr.left;
+                    prev=curr.left;
+                }
+                if (curr.right!=null){
+                    if (prev!=null)
+                        prev.next=curr.right;
+                    else 
+                        head=curr.right;
+                    prev=curr.right;
+                }
+                curr=curr.next;
+            }
+        }
+    }
 }
