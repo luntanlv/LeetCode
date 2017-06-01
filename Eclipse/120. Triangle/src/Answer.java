9dd98e2eafef6e15b4bcbfdcb01fc62d1acd1cdd
luntanlv <luntanlv@gmail.com>
2017-05-31 21:31:35 -0400

    120

--- Eclipse/120. Triangle/src/Answer.java
+++ Eclipse/120. Triangle/src/Answer.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Answer {
@@ -5,27 +6,54 @@ public class Answer {
 	{
         Answer answer = new Answer();
         
-        //List<List<Integer>> res = answer.generate(6);
-        List<Integer> res = answer.getRow(5);
+        List<List<Integer>> input = new ArrayList<List<Integer>>();
+        List<Integer> level1=new ArrayList<Integer>();
+        level1.add(-1);
+        List<Integer> level2=new ArrayList<Integer>();
+        level2.add(-2);
+        level2.add(-3);
+        
+        input.add(level1);
+        input.add(level2);
+        
+        int res = answer.minimumTotal(input);
         System.out.println(res);       
 	}
 	
+	
+	//This is Top down, Bottom up is easier
     public int minimumTotal(List<List<Integer>> triangle) {
     	//the level num, also the number of item in bottom level
     	int levelNum = triangle.size();
         int[] dp = new int[levelNum];
+        int minimumTotal = 0;
         
         for(int i=0; i<levelNum; i++){
+        	minimumTotal = Integer.MAX_VALUE;
         	List<Integer> level = triangle.get(i);
-        	for(int j=0; j<level.size(); j++){
-        		int pre =0;
-        		if(j-1<0)
-        			pre=dp[j];
-        		if(j>i-1)
-        			pre=dp[j-1];
-        		dp[j]=Math.min(dp[j], dp[j-1])+level.get(j);
+        	for(int j=level.size()-1; j>=0; j--){
+        		int left=0, right =0;
+        		if(j-1<0){
+        			left=dp[j];
+        			right=dp[j];
+        		}
+    			else if(j>i-1){
+    				left=dp[j-1];
+    				right=dp[j-1];
+    			}
+    			else{
+    				left = dp[j-1];
+    				right = dp[j];
+    			}
+    			
+        		dp[j]=Math.min(left, right)+level.get(j);
+        		minimumTotal = dp[j]<minimumTotal? dp[j]: minimumTotal;
         	}
         }
         
+        return minimumTotal;
+        
     }
+    
+    
 }
