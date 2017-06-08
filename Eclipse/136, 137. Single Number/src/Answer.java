28cabc6b3e80710e59fb11529df1c8bfdcdf7270
luntanlv <luntanlv@gmail.com>
2017-06-08 11:17:56 -0400

    136

---
+++ Eclipse/136, 137. Single Number/src/Answer.java
@@ -0,0 +1,29 @@
+
+public class Answer {
+	public static void main(String [] args)
+	{
+        Answer answer = new Answer(); 
+        int[] input = new int[]{1,2,2,0,1};
+        int res = answer.singleNumber(input);
+        System.out.println(res);       
+	}
+	
+	//136 :  every element appears two times except for one
+	//I saw the solution
+	// Logic: XOR will return 1 only on two different bits. So if two numbers are the same, XOR will return 0. Finally only one number left.
+	//   A ^ A = 0 and A ^ B ^ A = B.
+    public int singleNumber_2times(int[] nums) {
+    	int n = nums.length;
+        int result=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            result= result^nums[i];  /* Get the xor of all elements */
+        }
+        return result;
+    }
+    
+    //136 :  every element appears three times except for one
+    public int singleNumber_3times(int[] nums) {
+        
+    }
+}
