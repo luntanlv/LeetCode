c9ed2ccba64032beb3d70be869d482d4b69d98d6
luntanlv <luntanlv@gmail.com>
2017-06-08 16:26:18 -0400

    206

--- Visual Studio/206. Reverse Linked List/206. Reverse Linked List/Program.cs
+++ Visual Studio/206. Reverse Linked List/206. Reverse Linked List/Program.cs
@@ -10,6 +10,66 @@ namespace _206.Reverse_Linked_List
     {
         static void Main(string[] args)
         {
+            var node1 = new ListNode(1);
+            var node2 = new ListNode(2);
+            var node3 = new ListNode(3);
+
+            node1.next = node2;
+            node2.next = node3;
+
+            var res = ReverseList_better(node1);
+
+        }
+
+        //O(1) space
+        public static ListNode ReverseList_better(ListNode head)
+        {
+            ListNode cur = head, pre = null;
+
+            while (cur != null)
+            {
+                var next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+
+            return pre;
+        }
+
+        //O(n) space
+        public static ListNode ReverseList(ListNode head)
+        {
+            var st = new Stack<ListNode>();
+
+            var cur = head;
+            while (cur != null)
+            {
+                st.Push(cur);
+                cur = cur.next;
+            }
+
+            var fakeNewHead = new ListNode(0);
+            ListNode pre = fakeNewHead;
+            while (st.Count>0)
+            {
+                var curR = st.Pop();
+                pre.next = curR;                    
+                pre = curR;
+
+                if (st.Count == 0)
+                    curR.next = null;
+            }
+
+            return fakeNewHead.next;
         }
     }
+
+    //Definition for singly-linked list.
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
 }
