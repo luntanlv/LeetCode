2bd69c7284956d255ab09318a4e34cffed177fad
luntanlv <luntanlv@gmail.com>
2017-06-18 20:57:00 -0400

    238

---
+++ Eclipse/238. Product of Array Except Self/src/Answer.java
@@ -0,0 +1,15 @@
+
+public class Answer {
+
+	public static void main(String [] args)
+	{
+        Answer answer = new Answer(); 
+        int[] input = new int[]{1,2,2,0,1};
+        int[] res = answer.productExceptSelf(input);
+        System.out.println(res);       
+	}
+	
+    public int[] productExceptSelf(int[] nums) {
+        
+    }
+}
