04a3e4ad4e8c96d570b36bf831c362b380273535
luntanlv <luntanlv@gmail.com>
2017-06-20 10:44:25 -0400

    123

--- Eclipse/121, 122, 123 Best Time to Buy and Sell Stock/src/Answer.java
+++ Eclipse/121, 122, 123 Best Time to Buy and Sell Stock/src/Answer.java
@@ -5,7 +5,7 @@ public class Answer {
 	{
         Answer answer = new Answer();
         int[] input = new int[]{1,2,4,2,5,7,2,4,9,0};
-        int res = answer.maxProfit_123_twotravesal(input);
+        int res = answer.maxProfit_Dp(input);
         System.out.println(res);       
 	}
 	
@@ -51,30 +51,51 @@ public class Answer {
     
     //123
     //DP
+    //Idea is the same as 121, just loop k times, kth is based on (k-1)th
     //https://discuss.leetcode.com/topic/4766/a-clean-dp-solution-which-generalizes-to-k-transactions
-    // I got this from sell1 buy1 sell2 buy2 solution, I make it k
+    //https://discuss.leetcode.com/topic/8994/dp-o-kn-time-o-n-space-cpp-solution
     public int maxProfit_Dp(int[] prices) {
-    	int n = prices.length;
-    	int knum =2;
+    	int kNum=2;
+    	int n=prices.length;
     	
-    	//maxMoney_buy[k-1] Store the max money in the pocket after kth buy, it store previous value while i from 0 to n
-    	int[] maxMoney_buy = new int[knum];
-    	//maxMoney_sell[k-1] Store the max money in the pocket after kth sell
-    	int[] maxMoney_sell = new int[knum];
-    	
-		for(int k=0; k<knum; k++){
-	    	maxMoney_buy[k]= Integer.MIN_VALUE;
-	    	maxMoney_sell[k] = 0;
-		} 	
+    	//if k >= n/2, we can have transactions any time, it can be solved by O(n).
+        if (kNum > n / 2) {
+            int sum = 0;
+            for (int i = 1; i < n; i++){
+                if (prices[i] > prices[i - 1]){
+                    sum += prices[i] - prices[i - 1];
+                }
+            }
+            return sum;
+        }
+        
+        //use dp[k][i] represents, The max profit of using [0,i] data and at most k transactions.
+        //dp[k][i] = max( dp[k][i-1], max(prices[i]-prices[j] + dp[k-1][j]))  j[0~i], when j=0, it contains dp[k-1][i], mean we don't do kth here.
+        //dp[0][i] = 0, 0 times transation makes 0 profit; 
+        //dp[k][0] = 0, if there is only one price data point you can't make any money no matter how many times you can trade
+        int[][] dp = new int[kNum+1][n];
+        int maxProf = 0;
+        
+        for(int k=1; k<=kNum; k++){
 
-		for (int i = 0; i < n; i++) {
-			for(int k=0; k<knum; k++){
-				maxMoney_buy[k] = Math.max(maxMoney_buy[k], k>0? maxMoney_sell[k-1] - prices[i] : - prices[i]);
-				maxMoney_sell[k] = Math.max(maxMoney_sell[k], maxMoney_buy[k] + prices[i]);
-			}
-		}
-		return maxMoney_sell[knum-1];
-	}  
+        	int j=0;
+        	int makeNewTrans_maxPro=0;
+        	
+        	for(int i=1; i<n; i++){
+        		
+        		//For kth buy find lowest buy point till i
+        		if(-prices[i]+dp[k-1][i] > -prices[j]+dp[k-1][j])
+        			j=i;      			
+        		
+        		makeNewTrans_maxPro = Math.max(makeNewTrans_maxPro, prices[i]-prices[j]+dp[k-1][j]);
+        		
+        		dp[k][i] = Math.max(dp[k][i-1], makeNewTrans_maxPro);
+        		maxProf = Math.max(dp[k][i], maxProf);
+        	}
+        }
+        
+    	return maxProf;
+    }
     
     
     //two traversal
@@ -157,5 +178,30 @@ public class Answer {
 			sell2 = Math.max(sell2, buy2 + prices[i]);
 		}
 		return sell2;
-	}    
+	} 
+    
+    //dp2
+    // I got this from sell1 buy1 sell2 buy2 solution, I make it k
+    public int maxProfit_Dp2(int[] prices) {
+    	int n = prices.length;
+    	int knum =2;
+    	
+    	//maxMoney_buy[k-1] Store the max money in the pocket after kth buy, it store previous value while i from 0 to n
+    	int[] maxMoney_buy = new int[knum];
+    	//maxMoney_sell[k-1] Store the max money in the pocket after kth sell
+    	int[] maxMoney_sell = new int[knum];
+    	
+		for(int k=0; k<knum; k++){
+	    	maxMoney_buy[k]= Integer.MIN_VALUE;
+	    	maxMoney_sell[k] = 0;
+		} 	
+
+		for (int i = 0; i < n; i++) {
+			for(int k=0; k<knum; k++){
+				maxMoney_buy[k] = Math.max(maxMoney_buy[k], k>0? maxMoney_sell[k-1] - prices[i] : - prices[i]);
+				maxMoney_sell[k] = Math.max(maxMoney_sell[k], maxMoney_buy[k] + prices[i]);
+			}
+		}
+		return maxMoney_sell[knum-1];
+	}  
 }
