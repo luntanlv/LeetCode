8cda659f882696c9e9fd6c463e90cac0075e84a3
luntanlv <luntanlv@gmail.com>
2017-06-21 08:25:03 -0400

    23

--- Eclipse/23. Merge k Sorted Lists/src/Answer.java
+++ Eclipse/23. Merge k Sorted Lists/src/Answer.java
@@ -52,4 +52,46 @@ public class Answer {
         
         return dummyHead.next;
     }
+    
+    //divide and concur
+    public ListNode mergeKLists_dv(ListNode[] lists) {
+        if(lists.length==0)
+            return null;
+    	return heler(lists, 0, lists.length-1);
+    } 
+    
+    private ListNode heler(ListNode[] lists, int start, int end){
+    	if(start >= end)
+    		return lists[start];
+
+    	int m = (end+start)/2;
+    	ListNode res1 = heler(lists, start, m);
+    	ListNode res2 = heler(lists, m+1, end);
+    	
+    	ListNode res = merge(res1, res2);
+    	
+    	return res;
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2){
+    	ListNode dummyHead = new ListNode(0);
+    	ListNode pre = dummyHead;
+    	
+    	while(l1!=null && l2!=null){
+    		if(l1.val<l2.val){
+    			pre.next = l1;
+    			pre = l1;
+    			l1=l1.next;
+    		}
+    		else{
+    			pre.next = l2;
+    			pre = l2;
+    			l2=l2.next;
+    		}
+    	}
+    	
+    	pre.next = l1 != null? l1:l2;
+    	return dummyHead.next;
+    }
+    
 }
