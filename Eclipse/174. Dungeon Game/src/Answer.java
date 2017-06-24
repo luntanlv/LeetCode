f644238f03118cbb3c6724ad7a783a40d562d197
luntanlv <luntanlv@gmail.com>
2017-06-23 21:00:48 -0400

    174

--- Eclipse/174. Dungeon Game/src/Answer.java
+++ Eclipse/174. Dungeon Game/src/Answer.java
@@ -3,7 +3,8 @@ public class Answer {
 	public static void main(String [] args)
 	{
         Answer answer = new Answer();
-        int[][] input = new int[][]{{-2, -3, 3},{-5, -10, 1},{10, 30, -5}};
+        //int[][] input = new int[][]{{-2, -3, 3},{-5, -10, 1},{10, 30, -5}};
+        int[][] input = new int[][]{{-2, -3}};
         int res =answer.calculateMinimumHP(input);
         System.out.println(res);       
 	}
@@ -14,21 +15,25 @@ public class Answer {
     	if(n>0)
     		m=dungeon[0].length;
     	
-    	//store the max val(least boold)
+    	//dp[i][j] store the least boold need from i,j to princess
         int[][] dp = new int[n][m];
         
-        for(int i=n; i>=0; i--){
-        	for(int j=m; j>=0; j--){
-        		int right = i+1>=m? Integer.MAX_VALUE: dp[i+1][j];
-        		int down = j+1>=n? Integer.MAX_VALUE: dp[i][j+1];
-        		
-        		int need = Math.min(-dungeon[i][j]+right, -dungeon[i][j]+down);
-        		if(need<=0)
-        			need =1;
-        		dp[i][j]=need;
+        for(int i=n-1; i>=0; i--){
+        	for(int j=m-1; j>=0; j--){
+        		if(i+1>=n && j+1>=m)
+        			dp[i][j] = -dungeon[i][j]+1;
+        		else if(i+1>=n)
+        			dp[i][j] = -dungeon[i][j]+dp[i][j+1];
+        		else if(j+1>=m)
+        			dp[i][j] = -dungeon[i][j]+dp[i+1][j];
+        		else
+        			dp[i][j] = -dungeon[i][j] + Math.min(dp[i][j+1], dp[i+1][j]);
+
+        		if(dp[i][j]<=0)
+        			dp[i][j] =1;
         	}
         }
         
-        return -dp[n-1][m-1];
+        return dp[0][0];
     }
 }
