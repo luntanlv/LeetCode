44394934e0219fd4fe3aff2f95841ca399b0de38
luntanlv <luntanlv@gmail.com>
2017-06-28 20:47:00 -0400

    15

---
+++ Eclipse/15. 3Sum/src/Answer.java
@@ -0,0 +1,14 @@
+
+public class Answer {
+	public static void main(String [] args)
+	{
+        Answer answer = new Answer(); 
+        int[] input = new int[]{1,2,2,0,1};
+        int res = answer.singleNumber(input);
+        System.out.println(res);       
+	}
+	
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+    }
+}
