647368b125573d226256aa3517bded66ab68914d
luntanlv <luntanlv@gmail.com>
2017-07-04 11:33:20 -0400

    333

--- Eclipse/333. Largest BST Subtree/src/Answer.java
+++ Eclipse/333. Largest BST Subtree/src/Answer.java
@@ -19,27 +19,40 @@ public class Answer {
         System.out.println(res);       
 	}
 	
-	//divide and concour
+	//Backtacking, visit each node once, so O(n) time
     public int largestBSTSubtree(TreeNode root) {
     	int[] res = helper(root);
     	return res[1];
     }
     
-    //0: isBST, 1:maxBST So far
+    //0: isBST, 1:maxBST count So far, 2:Min val from leafs to current node, 3: Max val from leafs to current node
     private int[] helper(TreeNode root){
-        if(root==null)
-        	return new int[]{1,0};
-
+        if(root == null)
+        	return new int[]{1,0,Integer.MAX_VALUE,Integer.MIN_VALUE};
+        
         int[] left = helper(root.left);
         int[] right = helper(root.right);
         
-        int leftVal = root.left == null ? Integer.MIN_VALUE: root.left.val;
-        int rightVal = root.right == null? Integer.MAX_VALUE: root.right.val;
-        
-        if(left[0]>0 && right[0]>0 && root.val>leftVal && root.val<rightVal){
-        	return new int[]{1, left[1] + right[1] +1};
+        //BST
+        if( (left[0]>0 && root.val>left[3]) && (right[0]>0 && root.val<right[2]) ){
+        	return new int[]{1, left[1] + right[1] +1, Math.min(root.val, left[2]), Math.max(root.val,right[3])};
         }
+        //Not BST, Max and Min doesn't matter anymore, set to 0 here.
         else
-        	return new int[]{0, Math.max(left[1], right[1])}; 	
+        	return new int[]{0, Math.max(left[1], right[1]),0,0}; 	
     }
+    
+//    private int[] helper(TreeNode root){
+//        if(root.left==null && root.right==null)
+//        	return new int[]{1,1,root.val,root.val};
+//        
+//        int[] left = root.left != null? helper(root.left) : new int[]{1,0,Integer.MAX_VALUE,Integer.MIN_VALUE};
+//        int[] right = root.right !=null? helper(root.right) : new int[]{1,0,Integer.MAX_VALUE,Integer.MIN_VALUE};
+//        
+//        if( (left[0]>0 && root.val>left[3]) && (right[0]>0 && root.val<right[2]) ){
+//        	return new int[]{1, left[1] + right[1] +1, Math.min(root.val, left[2]), Math.max(root.val,right[3])};
+//        }
+//        else
+//        	return new int[]{0, Math.max(left[1], right[1]),0,0}; 	
+//    }
 }
