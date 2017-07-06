b745e0facbcce179579756a3b1cc351d77cdae5a
luntanlv <luntanlv@gmail.com>
2017-07-05 22:24:43 -0400

    621

--- Eclipse/621. Task Scheduler/src/Answer.java
+++ Eclipse/621. Task Scheduler/src/Answer.java
@@ -1,46 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Answer {
 	public static void main(String [] args)
 	{
         Answer answer = new Answer(); 
         char[] input = new char[]{'A','A','A','B','B','B'};
-        int res = answer.leastInterval(input, 2);
+        int res = answer.leastInterval(input, 50);
         System.out.print(res);
 	}
 	
+	//I saw the solution
+	//Not easy to undertand regards to how to tracking cooling time of tasks
+	//!!!!!!/Chose priority queue solution
+	//Now, the task picked up first after the sorting, 
+	//will either be the first task picked up in the last iteration(which will now be picked after its cooling time has been finished)
+	//
+	//or the task picked will be the one which lies at (n+1)th position in the previous descending tasks array.( Since all the count of  
+	//first n task in previous descending tasks array will be minus by 1£¬ so the new order will either stay the same or start with task at 
+	//(n+1)th position in the previous descending tasks array.
+	//
+	//In either of the cases, the cooling time won't cause any conflicts(it has been considered implicitly).
     public int leastInterval(char[] tasks, int n) {
-        int[] count = new int[26];
-        for(char task : tasks){
-        	int index = task - 'A';
-        	count[index]++;
+        int[] map = new int[26];
+        for (char c: tasks)
+            map[c - 'A']++;
+        Arrays.sort(map);
+        int time = 0;
+        while (map[25] > 0) {
+            int i = 0;
+            while (i <= n) {
+                if (map[25] == 0)
+                    break;
+                if (i < 26 && map[25 - i] > 0)
+                    map[25 - i]--;
+                time++;
+                i++;
+            }
+            Arrays.sort(map);
         }
-        
-        int curTaskCount = 0, totalTask = tasks.length;
-        int cpu =0;
-        
-        while(curTaskCount<totalTask){
-        	int index = 0;
-        	//any invalid index to start
-        	int preIndex = totalTask;
-        	
-        	for(int i=0; i<n; i++){
-        		while(count[index]==0){
-        			index = (index+1)%26;
-        		}
-        		
-        		if(index == preIndex){
-        			cpu+=n;
-        		}
-        		else{
-        			cpu++;
-        		}
-        		
-        		count[index]--;
-        		index++;
-        		curTaskCount++;
-        	}
-        }
-        
-        return cpu;
+        return time;
     }
 }
