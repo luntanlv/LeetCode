bcc7e8ac17c848f77b937a07a606c5136ad01938
luntanlv <luntanlv@gmail.com>
2017-07-12 17:23:59 -0400

    11

---
+++ Eclipse/11. Container With Most Water/src/Answer.java
@@ -0,0 +1,18 @@
+
+public class Answer {
+	public static void main(String [] args)
+	{
+        Answer answer = new Answer();      
+        int[] nums = new int[]{3,5,3};
+        int res = answer.maxArea(nums);
+        
+        System.out.println(res);    
+	}
+	
+	// I saw the solution\
+	// From DP to Greedy O(n)
+	// https://discuss.leetcode.com/topic/50763/from-dp-to-greedy-o-n-with-explanation-easy-way-to-see-this-problem
+    public int maxArea(int[] height) {
+        
+    }
+}
