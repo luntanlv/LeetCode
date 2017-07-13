09032dfe6ac1cb3323a9f9a2cb836b87b862e618
luntanlv <luntanlv@gmail.com>
2017-07-13 08:22:34 -0400

    11

--- Eclipse/11. Container With Most Water/src/Answer.java
+++ Eclipse/11. Container With Most Water/src/Answer.java
@@ -13,6 +13,16 @@ public class Answer {
 	// From DP to Greedy O(n)
 	// https://discuss.leetcode.com/topic/50763/from-dp-to-greedy-o-n-with-explanation-easy-way-to-see-this-problem
     public int maxArea(int[] height) {
-        
+        int l=0, r=height.length-1;
+        int max=0;
+        while(l<r){
+        	int curH = Math.min(height[l], height[r]);
+        	max=Math.max(max, curH*(r-l));
+        	if(curH==height[l])
+        		l++;
+        	else
+        		r--;
+        }
+        return max;
     }
 }
