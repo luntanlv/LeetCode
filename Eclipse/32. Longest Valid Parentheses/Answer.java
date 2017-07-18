04893265118ab5e6028d4ae90043bb520ab18a81
luntanlv <luntanlv@gmail.com>
2017-07-17 21:55:03 -0400

    32

---
+++ Eclipse/32. Longest Valid Parentheses/Answer.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Answer {
+	public static void main(String [] args)
+	{
+        Answer answer = new Answer();
+        List<String> res = answer.letterCombinations("23");
+        System.out.println(res);       
+	}
+	
+    public int longestValidParentheses(String s) {
+        
+    }
+}
