3876260c2b80c5d5e9c596bef13e725083c653f4
luntanlv <luntanlv@gmail.com>
2017-07-23 22:27:29 -0400

    41

--- Eclipse/41. First Missing Positive/src/Answer.java
+++ Eclipse/41. First Missing Positive/src/Answer.java
@@ -15,6 +15,7 @@ public class Answer {
     public int firstMissingPositive(int[] nums) {
 
         for(int i=0; i<nums.length; i++){
+        	// nums[i]!=nums[nums[i]-1] key point to avoid infinity loop, if on element already at right position, then skip, so loop won't happen
         	while(nums[i]>0 && nums[i]<=nums.length && nums[i]!=nums[nums[i]-1]){
         		int temp = nums[nums[i]-1];
         		nums[nums[i]-1] = nums[i];
