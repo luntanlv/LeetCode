1b336005701c44131628626575b01e53cea08c44
luntanlv <luntanlv@gmail.com>
2017-07-25 15:04:58 -0400

    48

--- Eclipse/48. Rotate Image/src/Answer.java
+++ Eclipse/48. Rotate Image/src/Answer.java
@@ -3,7 +3,7 @@ public class Answer {
 	{
         Answer answer = new Answer();
         int[][] input = {{1,0,0,1},{1,2,3,4},{1,2,4,3},{1,3,2,4}};
-        answer.rotate(input);
+        answer.rotate_better(input);
         for(int i = 0; i<input.length; i++){
         	for(int j=0; j<input[0].length; j++){
         		System.out.print(input[i][j] + ", ");
@@ -12,6 +12,28 @@ public class Answer {
         	
         }
 	}
+	
+	//two flip. 1 diagonal, 2. left right
+	//I saw the solution
+    public void rotate_better(int[][] matrix) {
+    	int n=matrix.length;    	
+        for(int i=0; i<n; i++){
+        	for(int j=0; j<i; j++){
+        		int temp = matrix[i][j];
+        		matrix[i][j]=matrix[j][i];
+        		matrix[j][i] = temp;
+        	}
+        }
+        
+        for(int i=0; i<n; i++){
+        	for(int j=0; j<n/2; j++){
+        		int temp=matrix[i][j];
+        		matrix[i][j]=matrix[i][n-1-j];
+        		matrix[i][n-1-j] = temp;
+        	}
+        }
+    }
+
 	
 	//swtich value from most out layer in the matrix to inner layer, 
 	//in each layer switch value by rotating clockwise 4 times, make value[j][n-i]=value[i][j] each time.
