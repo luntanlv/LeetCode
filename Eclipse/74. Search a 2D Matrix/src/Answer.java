276f3630ba1ddb63a912cd2b467f892ed4d18df2
luntanlv <luntanlv@gmail.com>
2017-07-31 11:28:08 -0400

    74

--- Eclipse/74. Search a 2D Matrix/src/Answer.java
+++ Eclipse/74. Search a 2D Matrix/src/Answer.java
@@ -16,6 +16,24 @@ public class Answer {
 	}
 	
     public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n=0;
+        if(m>0)
+        	n=matrix[0].length;
         
+        int start=0, end= m*n-1;
+        while(start<=end){
+        	int mid = start+ (end-start)/2;
+        	int x = mid/n;
+        	int y = mid%n;
+        	
+        	if(matrix[x][y]<target)
+        		start = mid+1;
+        	else if(matrix[x][y]>target)
+        		end = mid -1;
+        	else
+        		return true;       	
+        }
+        
+        return false;
     }
 }
