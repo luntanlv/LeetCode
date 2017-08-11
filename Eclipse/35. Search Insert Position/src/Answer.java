b7fe7e4e5845c2ebd7897e9c5c96f837dc3a4a98
luntanlv <luntanlv@gmail.com>
2017-08-11 10:38:10 -0400

    35

--- Eclipse/35. Search Insert Position/src/Answer.java
+++ Eclipse/35. Search Insert Position/src/Answer.java
@@ -9,6 +9,19 @@ public class Answer {
         System.out.println(res);    
 	}
 	
+    //Second round
+    public int searchInsert2(int[] A, int target) {
+        int low = 0, high = A.length-1;
+        while(low<high-1){
+            int mid = (low+high)/2;
+            if(A[mid] == target) return mid;
+            else if(A[mid] > target) high = mid;
+            else low = mid+1;
+        }
+        return A[low]>=target? low:  A[high]>=target? high: high+1;
+    }
+    
+    //First round
     public int searchInsert(int[] nums, int target) {
         int start=0, end=nums.length-1;
         while(start<end){
