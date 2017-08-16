5ddda4fa0fc963f9d3647ce9d862356f9af30695
luntanlv <luntanlv@gmail.com>
2017-08-15 23:36:47 -0400

    132

---
+++ Eclipse/132. Palindrome Partitioning II/src/Answer.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class Answer {
+	public static void main(String [] args)
+	{
+        Answer answer = new Answer();   
+        int res = answer.minCut("aabb");
+        System.out.println(res);       
+	}
+	
+	//similar to 5.Longest Palindromic Substring;
+	//I saw the solution
+    public int minCut(String s) {
+    	int n=s.length();
+    	//isp[i][j] indicate whether s.subString(i,j+1) is palindrome
+        boolean[][] isp = new boolean[n][n];
+        //cut[i]: min cut for s.subString(i,n)
+        int[] cut = new int[n];
+        
+        char[] sch = s.toCharArray();
+        for(int i=n-1; i>=0; i--){
+        	////if s.substring(j+1, n) at least will be palindrome if we divide all partition to a single char
+        	int curC=n-i-1;
+        	
+        	for(int j=i; j<n; j++){
+        		
+        		if(sch[i]==sch[j] && (j-i<=2 || isp[i+1][j-1]==true) ){
+        			isp[i][j]=true;
+        			if(j==n-1)
+        				curC = 0;
+        			else{
+        				//if s.substring(j+1, n) at least will be palindrome if we divide all partition to a single char
+        				curC = Math.min(curC, cut[j+1]+1);
+        			}
+        		}
+        	}
+        	cut[i]=curC;
+        }
+        return cut[0];
+    }
+}
--- Eclipse/135. Candy/src/Answer.java
+++ Eclipse/135. Candy/src/Answer.java
@@ -1,10 +1,11 @@
+import java.util.Stack;
 
 public class Answer {
 	public static void main(String [] args)
 	{
         Answer answer = new Answer(); 
-        int[] ratings = new int[]{4,5,3,2};
-        int res = answer.candy(ratings);
+        int[] ratings = new int[]{1,2,5};
+        int res = answer.candy_st(ratings);
         System.out.println(res);       
 	}
 	
@@ -24,5 +25,38 @@ public class Answer {
     	for(int count: candy)
     		res+=count;
     	return res;
+    }
+    
+    //I saw the solution,
+    //O(1) space, the idea of using stack
+    public int count(int n) {
+        return (n * (n + 1)) / 2;
+    }
+    public int candy_o1(int[] ratings) {
+        if (ratings.length <= 1) {
+            return ratings.length;
+        }
+        int candies = 0;
+        int up = 0;
+        int down = 0;
+        int old_slope = 0;
+        for (int i = 1; i < ratings.length; i++) {
+            int new_slope = (ratings[i] > ratings[i - 1]) ? 1 : (ratings[i] < ratings[i - 1] ? -1 : 0);
+            if ((old_slope > 0 && new_slope == 0) || (old_slope < 0 && new_slope >= 0)) {
+                candies += count(up) + count(down) + Math.max(up, down);
+                up = 0;
+                down = 0;
+            }
+            if (new_slope > 0)
+                up++;
+            if (new_slope < 0)
+                down++;
+            if (new_slope == 0)
+                candies++;
+
+            old_slope = new_slope;
+        }
+        candies += count(up) + count(down) + Math.max(up, down) + 1;
+        return candies;
     }
 }
