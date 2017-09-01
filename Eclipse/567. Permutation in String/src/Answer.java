a0860f86c119d551619894a688eb6092c9710a1f
luntanlv <luntanlv@gmail.com>
2017-08-31 22:28:18 -0400

    567

--- Eclipse/567. Permutation in String/src/Answer.java
+++ Eclipse/567. Permutation in String/src/Answer.java
@@ -4,10 +4,57 @@ public class Answer {
 	public static void main(String [] args)
 	{
         Answer answer = new Answer();      
-        boolean res = answer.checkInclusion("a", "ab");
+        boolean res = answer.checkInclusion("abc", "bbbca");
         System.out.print(res);
 	}
 	
+	//Second Round
+	//Sliding window
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        
+        char[] s1c = s1.toCharArray();
+        char[] s2c = s2.toCharArray();
+        
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1c[i] - 'a']++;
+        }
+        
+        int count = s1c.length;
+        int[] s1mapCur = Arrays.copyOf(s1map, s1map.length);
+        
+        int li=0, ri=0;
+        while(ri<s2c.length){
+        	int r = s2c[ri]-'a';
+        	
+        	if(s1mapCur[r]>0){
+        		count--;
+        		s1mapCur[r]--;
+        		
+        		if(count==0)
+        			return true;
+        		
+        		ri++;
+        	}
+        	else if(s1mapCur[r]==0 && s1map[r]>0){   		
+        		while(s1mapCur[r]==0){
+        			int l = s2c[li]-'a';
+        			s1mapCur[l]++;
+        			li++;
+        			count++;
+        		}
+        	}
+        	else{
+        		ri++;
+        		li=ri;
+                count = s1c.length;
+                s1mapCur = Arrays.copyOf(s1map, s1map.length); 		
+        	}    	
+        }
+        return count==0;
+    }
 	
 	//I saw the solution	
 	//Sort and compare. [Time Limit Exceeded]
@@ -30,7 +77,7 @@ public class Answer {
     //I saw the solution
     //Sliding Window
     //one string will be a permutation of another string only if both of them contain the same charaters with the same frequency
-    public boolean checkInclusion(String s1, String s2) {
+    public boolean checkInclusion_sw(String s1, String s2) {
         if (s1.length() > s2.length())
             return false;
         int[] s1map = new int[26];
